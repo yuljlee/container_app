3b9c4fb63dba4d45e005b86725e626b7ea69de1b
Yul Jae Lee <yuljlee9@gmail.com>
2020-01-31 21:44:40 -0800

    feat: Add gradient

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,21 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Container'),
         ),
-        body: Container(
-          color: Colors.orange,
-        ),
+        body: 
+          Container(
+            margin: EdgeInsets.all(100.0),
+            width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple[50], Colors.purple[500]]),
+              color: Colors.orange,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0)
+              )
+            ),
+        ),        
       ),
     );
   }
